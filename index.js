d7840dae21d7ae3971b5ee788b75561ffe7e9000
semih Fatih <s_fatihh@hotmail.com>
2018-11-07 11:42:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  let elements = document.querySelectorAll('.ranked-list li');
+   
+   for(let i=0; i<elements.length; i++){
+     elements[i].innerHTML = parseInt(elements[i].innerHTML) + n;
+   }
+  
+}
+
+function deepestChild(){
+  let node = document.querySelector('div#grand-node');
+  
+  while(node.children.length != 0){
+    let temp = node.children[0];
+    node = temp.children[0];
+    
+  }
+  return node;
+}
